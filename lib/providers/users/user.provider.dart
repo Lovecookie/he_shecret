3c03cf19999cd847b51d06ca94526d6fb6cdb0b2
BaekSeungmin <silver2000cs@gmail.com>
2023-05-22 13:58:58 +0900

    연계 수정

--- lib/providers/users/user.provider.dart
+++ lib/providers/users/user.provider.dart
@@ -1,3 +1,4 @@
+import 'package:fixnum/fixnum.dart';
 import 'package:shipcret/providers/entitys/user.entity.dart';
 import 'package:shipcret/providers/entitys/user_profile.entity.dart';
 import 'package:shipcret/providers/users/user_repository.dart';
@@ -5,11 +6,22 @@ import 'package:shipcret/providers/users/user_service.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final userServiceProvider = StateNotifierProvider<FUserServiceState, FUserEntity>((ref) {
+final myUserServiceProvider = StateNotifierProvider<FUserServiceState, FUserEntity>((ref) {
   return FUserServiceState();
 });
 
-final userProfileProvider = FutureProvider.autoDispose<FUserProfileEntity>((ref) async {
+final myUserFutureProvider = FutureProvider.autoDispose<FUserEntity>((ref) async {
+  final optional = await ref.read(userRepository).myInfo();
+  if (!optional.hasValue) {
+    return FUserEntity();
+  }
+
+  ref.read(myUserServiceProvider.notifier).setUserEntity(FUserEntity.fromJson(optional.value.toJson()));
+
+  return FUserEntity.fromJson(optional.value.toJson());
+});
+
+final userProfileProvider = FutureProvider.autoDispose.family<FUserProfileEntity, Int64>((ref, useruuid) async {
   final optional = await ref.read(userRepository).myProfile();
   if (!optional.hasValue) {
     return FUserProfileEntity();
--- lib/providers/users/user_service.dart
+++ lib/providers/users/user_service.dart
@@ -1,13 +1,29 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shipcret/providers/entitys/user.entity.dart';
+import 'package:shipcret/providers/entitys/user_profile.entity.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FUserServiceState extends StateNotifier<FUserEntity> {
   FUserServiceState() : super(FUserEntity());
 
   FUserEntity get userEntity => state;
-  set setUserEntity(FUserEntity userEntity) => state = userEntity;
+
+  void setUserEntity(FUserEntity userEntity) {
+    state = userEntity;
+  }
 
   void clear() {
     state = FUserEntity();
+  }
+}
+
+class FUserProfileInfoServiceState extends StateNotifier<FUserProfileEntity> {
+  FUserProfileInfoServiceState() : super(FUserProfileEntity());
+
+  FUserProfileEntity get userProfileEntity => state;
+  set setUserEntity(FUserProfileEntity userEntity) => state = userEntity;
+
+  void clear() {
+    state = FUserProfileEntity();
   }
 }
